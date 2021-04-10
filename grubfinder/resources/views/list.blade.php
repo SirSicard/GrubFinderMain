5ffc984cf784b5a527887c4fade64b580523359a
Rick <rickoliwer@gmail.com>
2021-04-10 15:56:03 +0200

    Added diffForHumans() to restaurant list

--- grubfinder/resources/views/list.blade.php
+++ grubfinder/resources/views/list.blade.php
@@ -3,27 +3,30 @@
 
 <section class="py-8 bg-white">
     {!! Form::open(['route' => 'filter', 'class' => 'space-y-5']) !!}
-    <div class="flex grid w-3/4 grid-cols-3 m-auto">
+        <div class="flex flex-wrap items-center justify-between w-3/4 m-auto">
 
-        <div>
-            {{ Form::select('location', $locations, ['class' => 'w-full', 'id' => 'location']) }}
+            <div>
+                {{ Form::select('category', $categories, ['class' => 'w-full', 'id' => 'category'])  }}
+            </div>
+
+            <div>
+                {{ Form::select('location', $locations, ['class' => 'w-full', 'id' => 'location']) }}
+            </div>
+            
+            <div>
+                {{ Form::select('category', $categories, ['class' => 'w-full', 'id' => 'category'])  }}
+            </div>
+            <div>
+                <button class="">
+                    <div class="block inline-block float-right p-4 text-blue-900 transition duration-300 ease-out transform bg-blue-400 rounded hover:bg-blue-300 hover:text-blue-800 hover:shadow-inner hover:scale-105 hover:bg-opacity-50">
+                        <svg  xmlns="http://www.w3.org/2000/svg" class="inline-block w-4 pb-1" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>Search
+                    </div>
+                </button>
+            </div>
         </div>
-  
-
-        <div>
-            {{ Form::select('category', $categories, ['class' => 'w-full', 'id' => 'category'])  }}
-        </div>
-
-
-    <button class="float-right">
-        <div class="font-bold button">
-            <svg  xmlns="http://www.w3.org/2000/svg" class="inline-block w-4 pb-1" fill="none" viewBox="0 0 24 24"
-                  stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>Search
-        </div>
-    </button>
-  </div>
     {!! Form::close() !!}
 </section>
 <section class="px-12 py-8 bg-white border-b">   
@@ -42,7 +45,7 @@
                 <span class="block text-sm text-gray-500">{{$restaurant->categories->implode('name', ', ')}}</span>
             </div>
             <div class="absolute top-0 p-2 mt-2 ml-2 text-xs uppercase bg-gray-200 rounded-full bg-secondary-100 text-secondary-200">
-                <span>{{$restaurant->updated_at}}</span>
+            <time datetime="{{ $restaurant->created_at->toIso8601String() }}">{{ $restaurant->created_at->diffForHumans() }}</time>
             </div>
         </div>
         @endforeach 
