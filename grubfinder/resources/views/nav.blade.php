dbe4f7f7c8867870615727ca62ea29d01daf2cf0
Mattias Herzig <Mattiasherzig@hotmail.com>
2021-04-11 20:20:01 +0200

    css fixes

--- grubfinder/resources/views/nav.blade.php
+++ grubfinder/resources/views/nav.blade.php
@@ -31,17 +31,6 @@
           </a>
         </div>
 
-        <!-- Search bar -->
-        <div class="relative pt-2 mx-auto text-gray-600">
-          <input class="h-10 px-5 pr-16 text-sm bg-white border-2 border-gray-300 rounded-lg focus:outline-none"
-            type="search" name="search" placeholder="Search">
-          <button type="submit" class="absolute top-0 right-0 mt-5 mr-4">
-            <svg class="w-4 h-4 text-gray-600 fill-current" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve" width="512px" height="512px">
-              <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"></svg>
-          </button>
-        </div>
-        <!-- End of Search bar -->
-
         <!-- Navbar -->
         <div class="block pr-4 lg:hidden">
           <button id="nav-toggle" class="flex items-center px-3 py-2 text-gray-500 border border-gray-600 rounded appearance-none hover:text-gray-800 hover:border-green-500 focus:outline-none">
@@ -58,7 +47,7 @@
               <a class="inline-block py-2 text-black hover:text-gray-800 hover:underline" href="{{ route('register') }}">Apply for admin</a>
             </li>
             <li class="mr-3">
-              <a class="inline-block px-4 py-2 text-black hover:text-gray-800 hover:underline" href="{{ route('add') }}">Sugestions</a>
+              <a class="inline-block px-4 py-2 text-black hover:text-gray-800 hover:underline" href="{{ route('add') }}">Suggestions</a>
             </li>
           </ul>
           <button id="navAction" class="px-8 py-4 mx-auto mt-4 font-extrabold text-gray-800 rounded shadow opacity-75 lg:mx-0 hover:underline lg:mt-0"><a href="{{ route('login') }}">Admin</a></button>
